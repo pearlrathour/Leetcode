44911296485a61bb1d15d9e243185f61e7541601
Pearl Rathour <99817664+pearlrathour@users.noreply.github.com>
2022-10-02 23:51:54 +0530

    Time: 8 ms (10.52%), Space: 6.5 MB (51.49%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        bool flag=false;
+        for(int i=s.length()-1 ; i>=0 ; i--){
+            if(s[i]!=' '){
+                count++;
+                flag=true;
+            }
+            else{
+            if(flag==true)
+                return count;
+            }
+        }
+        return count;
+    }
+};

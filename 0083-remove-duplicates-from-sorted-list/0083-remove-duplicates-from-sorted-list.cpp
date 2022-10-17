0ef77668f6557b741a8eacbb5e2662c20e6b8f9a
Pearl Rathour <99817664+pearlrathour@users.noreply.github.com>
2022-10-17 21:05:07 +0530

    Time: 15 ms (74.20%), Space: 11.8 MB (7.87%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -14,7 +14,9 @@ public:
         ListNode *curr=head;
         while(curr!=NULL && curr->next!=NULL){
             if(curr->val == curr->next->val){
+                ListNode *temp=curr->next;
                 curr->next=curr->next->next;
+                delete(temp);
             }
             else
                 curr=curr->next;
